df7938e2426e3033b655303fc6ab33eea21bcc7c
John Hancock <jdhancock@gmail.com>
2015-03-25 09:10:17 -0500

    Initial commit
    Setting up the project folder.

---
+++ js/actions-v0.js
@@ -0,0 +1,32 @@
+		jQuery(document).ready(function ($) {
+
+			var simpleDisplayAction = $('.simple-reveal-action');
+
+			function displayInit(){
+				if (simpleDisplayAction.length){
+					$.getScript("/interactives/template/common/js/kason.display.js", function(){
+						simpleDisplayAction.kason_display();
+					});
+				}
+			}
+
+			displayInit();
+
+
+		});
+
+
+
+function init(){
+	if (storyURL && comments){
+		commentInit(storyURL, storyTitle);
+	}
+	if (storyURL && leadText && share){
+		shareInit(storyURL, storyTitle, leadText, storyIMG, 'share-bar1');
+		shareInit(storyURL, storyTitle, leadText, storyIMG, 'share-bar2');
+
+	}
+	// trackingInit(storyURL);
+	
+}
+init();
