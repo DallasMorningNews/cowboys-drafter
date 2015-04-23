b9647c4cc8e911a582a809478b7ffdf7070c6082
John Hancock <jdhancock@gmail.com>
2015-04-23 08:57:28 -0500

    Updated player pool, small wording change
    — Updated the player pool so that every player has a blurb now.
    — Fixed a small wording issue on position intros

--- js/custom.js
+++ js/custom.js
@@ -111,7 +111,7 @@ $(document).ready(function () {
 
 				// if there's a blurb for a player (written by Sturm or the staff), add that blurb to the player's module
 				if (playerPool[key].sturmsays) {
-					playerModules += "<p class='blurb'><strong>BOB STURM SAYS: </strong>" + value.sturmsays + " <a target='_blank' href='" + value.sturmlink + "'>Read More.</a></p>";
+					playerModules += "<p class='blurb'><strong>SCOUTING REPORT: </strong>" + value.sturmsays + " <a target='_blank' href='" + value.sturmlink + "'>Read More.</a></p>";
 				}
 				playerModules += "<a href='#round" + (round + 1) + "' class='roundSubmitter' data-round='round" + round + "' data-roundInt='" + round + "'>Draft " + value.firstname + " " + value.lastname + "</a>"
 				playerModules += "</div>";
